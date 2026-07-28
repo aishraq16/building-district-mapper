609a6d8bc5f071a3a869244089026bd47a668724
aishraq16 <ishraqafzal16@gmail.com>
2026-07-28 18:35:49 +0800

    switched to writing everything in one script for now

--- main.js
+++ main.js
@@ -26,3 +26,39 @@ try {
 } finally {
   await client.close();
 }
+
+function calculateHaversineDistance(coordA, coordB){
+
+}
+
+
+function getBuildingsMissingDistrictId(){
+
+  const result = await buildings.findOne({
+  $or: [
+    { districtId: { $exists: false } },
+    { districtId: null },
+    { districtId: "" }
+  ]
+});
+
+return result;
+}
+
+function getDistrictReferenceBuildings(){
+  //implement later
+}
+
+function findNearestReferenceBuildings(){
+  //implement later
+}
+
+function getMajorityDistrictVote(){
+  //implement later
+}
+
+function inferDistrictIdForBuilding(){
+  //implement later
+}
+
+//console.log(getBuildingsMissingDistrictId().countDocuments())
