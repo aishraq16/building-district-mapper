f23e446fabbcdbe656cc5068a19847cb5175d3ab
aishraq16 <ishraqafzal16@gmail.com>
2026-07-29 17:33:25 +0800

    small changes

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+//imports 
 import "dotenv/config";
 import { MongoClient } from "mongodb";
 import { distance } from "@turf/turf"
 
+//get the connection string and db name from the env file
 const connectionURI = process.env.MONGO_URI;
 const dbName = process.env.MONGO_DB;
 
+//error handling for missing connection string / db name variables
 if (!connectionURI) {
   throw new Error("Missing MONGO_URI in environment variables");
 }
@@ -13,6 +16,7 @@ if (!dbName) {
   throw new Error("Missing MONGO_DB in environment variables");
 }
 
+//connect to mongodb
 const client = new MongoClient(connectionURI);
 
 await client.connect();
@@ -20,12 +24,22 @@ console.log("Connected to MongoDB");
 
 const db = client.db(dbName);
 const buildings = db.collection("property_buildings");
-
+/*
 const count = await buildings.countDocuments();
 console.log("Buildings count:", count);
+*/
+
+//helper functions for this task
+
+function getCoordinates(building){
+  return { 
+    "lattitude" : building.location.geolocation.latitude,
+    "longitude" : building.location.geolocation.longitude
+   }
+}
 
 async function getBuildingsMissingDistrictId(){
-
+//
   const result = await buildings.find({
   $or: [
     { districtId: { $exists: false } },
@@ -40,7 +54,6 @@ return result;
 }
 
 async function getDistrictReferenceBuildings(){
-  //implement later
   const result = await buildings.find({
   districtId: { $exists: true, $nin: [null, ""] },
   "location.geolocation.latitude": { $type: "number" },
